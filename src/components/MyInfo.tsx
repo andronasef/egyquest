ac535e8302e8b4b7b998e2ed122c70da91251d93
Andrew Nasef <androamazing@gmail.com>
2023-04-14 12:21:58 +0000

    feat: 🎨 add link to info box

--- src/components/MyInfo.tsx
+++ src/components/MyInfo.tsx
@@ -2,9 +2,11 @@ import MaterialSymbolsInfo from '~icons/material-symbols/info';
 
 function MyInfo() {
   return (
-    <button className="absolute text-2xl rounded-full shadow text-white/90 md:text-3xl bottom-3 left-3">
-      <MaterialSymbolsInfo />
-    </button>
+    <a href="https://andronasef.github.io/projects/en/egyquest">
+      <button className="absolute text-2xl rounded-full shadow text-white/90 md:text-3xl bottom-3 left-3">
+        <MaterialSymbolsInfo />
+      </button>
+    </a>
   );
 }
 export default MyInfo;
