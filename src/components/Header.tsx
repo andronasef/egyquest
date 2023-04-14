e850ed69b2422f3c5aa529d72171ec8d91608f32
Andrew Nasef <androamazing@gmail.com>
2023-04-14 12:38:19 +0000

    refactor: ✨ no more text selection

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ function Header() {
   return (
     <>
       <header className="flex flex-row items-center justify-between h-16 px-5 text-white bg-surface">
-        <h1 className="text-2xl font-bold text-white select-none">EGYQUEST</h1>
+        <h1 className="text-2xl font-bold text-white">EGYQUEST</h1>
         <button
           disabled={status == LoadingStatus.LOADING}
           className="bg-primary text-white font-bold h-[2.5rem] px-3 rounded-md flex justify-center items-center gap-2 disabled:hover:bg-primary"
