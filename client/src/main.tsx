df393d3bc0f6db127f7e312ace746633b33b105f
Andrew Nasef <androamazing@gmail.com>
2023-07-02 12:43:31 +0000

    feat: add google analytics

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,11 @@
 import ReactDOM from 'react-dom/client';
+import ReactGA from 'react-ga4';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './store';
 import './styles/index.css';
+
+ReactGA.initialize('G-NE7GYZLCJT');
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   // <React.StrictMode>
